409a64b8609a8560951a3c734049e423b40f50d1
Gabrielle <88905557+command-gab@users.noreply.github.com>
2021-10-25 10:51:45 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : "", 
+    'url' : "mongodb+srv://commandgab:Testing4@cluster0.cev5m.mongodb.net/RouletteApp?retryWrites=true&w=majority", 
     'dbName': 'RouletteApp'
 };
